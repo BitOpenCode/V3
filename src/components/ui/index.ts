d3f5f0935c6661696e98337b9465f653de15d3fb
BitOpenCode <peteranbubit@gmail.com>
2026-01-07 19:54:02 +0300

    Cleanup: remove dead code, reduce CSS by 84%

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,3 @@
 // UI Components exports
 export { default as BubbleButton } from './BubbleButton';
 export { default as BubbleCard } from './BubbleCard';
-export { default as SocialCard } from './SocialCard';
-
